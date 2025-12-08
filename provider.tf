ff994c9a037072a31cae3707313ffd5863d6e5a2
SIVA PRASAD <golisiva50@gmail.com>
2025-12-08 20:32:28 +0530

    1st commit from local!!

---
+++ provider.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  resource_provider_registrations = "none"
+}
